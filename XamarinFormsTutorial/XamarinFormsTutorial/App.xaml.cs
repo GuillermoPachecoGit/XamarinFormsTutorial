706c80edf5625b1c9f7f784aab00361ff2e9ff77
guillermopacheco.exa <guillermopacheco.exa@gmail.com>
2021-02-23 14:59:04 -0300

    init

---
+++ XamarinFormsTutorial/XamarinFormsTutorial/App.xaml.cs
@@ -0,0 +1,34 @@
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+using XamarinFormsTutorial.ViewModels;
+using XamarinFormsTutorial.Views;
+
+namespace XamarinFormsTutorial
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
+        }
+    }
+}
---
+++ XamarinFormsTutorial/XamarinFormsTutorial/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinFormsTutorial.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+            TapCommand = new Command(async () => await TapCommandExecuteAsync());
+        }
+
+        private async System.Threading.Tasks.Task TapCommandExecuteAsync()
+        {
+            await NavigationService.NavigateAsync("HomePage");
+        }
+
+        public ICommand TapCommand { get; set; }
+    }
+}
