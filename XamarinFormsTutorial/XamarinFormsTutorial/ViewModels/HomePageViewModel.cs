e6c63cfdd1e3f7ff8c50b26f0234a2dd698b548a
guillermopacheco.exa <guillermopacheco.exa@gmail.com>
2021-02-25 01:13:38 -0300

    Bindings - Add Click counter

--- XamarinFormsTutorial/XamarinFormsTutorial/ViewModels/HomePageViewModel.cs
+++ XamarinFormsTutorial/XamarinFormsTutorial/ViewModels/HomePageViewModel.cs
@@ -1,16 +1,39 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace XamarinFormsTutorial.ViewModels
 {
-    public class HomePageViewModel : BindableBase
+    public class HomePageViewModel : ViewModelBase
     {
-        public HomePageViewModel()
-        {
-
+        private int _counter = 0;
+        public HomePageViewModel(INavigationService navigationService)
+            : base(navigationService) {
+            // AddClickCommand = new Command(AddClikCommandExecuteAsync);
+            // ClickCounterTitle = $"Clicks 0";
         }
+
+        /*
+        private void AddClikCommandExecuteAsync(object obj)
+        {
+            ClickCounterTitle = $"Clicks {_counter+=1}";
+        }
+
+        private string _clickCounterTitle;
+        public string ClickCounterTitle
+        {
+            get { return _clickCounterTitle; }
+
+            set { _clickCounterTitle = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand AddClickCommand { get; set; }*/
     }
 }
